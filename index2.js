796453e28fef4ab2c3b7fc3f76c75e2469091d33
mamoonatahir <mamoona.tahir3@gmail.com>
2020-12-20 00:33:06 +0500

    java assignment 1

---
+++ index2.js
@@ -0,0 +1,9 @@
+var Name = "Mamoona Tahir";
+alert(Name);
+var MyAge = "I am 35 years Old";
+ alert(MyAge);
+ var MyQualification = "Certified Mobile Application Development";
+var email = "mamoona.tahir3@gmail.com";
+var message = "my Email address is "
+alert(MyQualification);
+alert(message + email);
---
+++ index4.js
@@ -0,0 +1,5 @@
+var a = 4.4;
+var b = "5.3";
+var c = a + parseFloat(b);
+console.log(c);
+
